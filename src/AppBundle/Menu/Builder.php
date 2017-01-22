438a8e24f3ae561a73a9e5959e2042e622ef5768
Adrian Badarau <adi.badarau@yahoo.com>
2017-01-22 16:18:22 +0200

    refactored controllers and routing

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -16,7 +16,9 @@ class Builder
     public function mainMenu(MenuFactory $factory, array $options)
     {
         $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
         $menu->addChild('Home',['route' => 'homepage']);
+        $menu->addChild('Cars',['route' => 'car_index']);
 
         return $menu;
     }
